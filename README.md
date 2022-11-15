dd24fc5b79e09d57ec17de83ce46cfc8108a109f
AeahKa <aeah@qq.com>
2022-11-15 18:41:13 +0800

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+使用方法
+yarn global add parcel
+
+# 找个干净的目录
+
+git clone https://github.com/AeahKa/dom.git
+cd dom
+parcel src/index.html
--- dist/index.de158e3a.js
+++
@@ -1,5 +0,0 @@
-const $div = $("<div><span>1</span></div>");
-const $childList = $(".child");
-$("body").append($childList);
-
-//# sourceMappingURL=index.de158e3a.js.map
